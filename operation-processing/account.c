37cabfd0a1cd12bed41c54107de0909d2947131a
YordanTsonev <145608256+YordanTsonev@users.noreply.github.com>
2025-05-25 10:40:27 +0300

    Update account.c

--- operation-processing/account.c
+++ operation-processing/account.c
@@ -1,13 +1,13 @@
-#include "user.h"
+#include "account.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int find_account_by_index(User *user, int counter, const char *unique)
+int find_account_by_index(Account *account, int counter, const char *unique)
 {
     for(int i=0; i<counter; i++)
     {
-        if(strcmp(user[i].account_number,unique)==0)
+        if(strcmp(account[i].account_number,unique)==0)
         {
             return i;
         }
@@ -15,7 +15,7 @@ int find_account_by_index(User *user, int counter, const char *unique)
     return -1;
 }
 
-int load_accounts(const char *filename, User **user, const char, int *counter) 
+int load_accounts(const char *filename, Account **account, const char, int *counter) 
 {
     FILE *f = fopen(filename, "r");
     if(!f)
@@ -23,18 +23,18 @@ int load_accounts(const char *filename, User **user, const char, int *counter)
         return 0;
     }
     *counter = 0;
-    *user = NULL;
-    User var;
+    *account = NULL;
+    Account var;
     while(fscanf(f, "%s %lf %s", var.account_number, &var.current_balance, var.id)==3)
     {
-        User *new_array = realloc(*user,(*counter+1)*sizeof(User));
+        Account *new_array = realloc(*account,(*counter+1)*sizeof(Account));
         if(!new_array) 
         {
             fclose(f);
             return 0;
         }
-        *user = new_array;
-        (*user)[*counter] = temp;
+        *account = new_array;
+        (*account)[*counter] = temp;
         (*counter)++;
     }
     fclose(f);
@@ -42,7 +42,7 @@ int load_accounts(const char *filename, User **user, const char, int *counter)
 }
 
 
-int save_accounts(const char *filename, User *user, int counter)
+int save_accounts(const char *filename, Account *account, int counter)
 {
     FILE *f = fopen(filename, "w");
     if(!f) 
@@ -51,7 +51,7 @@ int save_accounts(const char *filename, User *user, int counter)
     }
     for(int i=0;i<counter;i++) 
     {
-        fprintf(f, "%s %.2f %s\n",user[i].account_number,user[i].current_balance,user[i].id);
+        fprintf(f, "%s %.2f %s\n",account[i].account_number,account[i].current_balance,account[i].id);
     }
     fclose(f);
     return 1;
